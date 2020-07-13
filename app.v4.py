bc83ba1940ec90b6a29ae15a75e7c4ec38acb983
Stephen Thomas <stevesargon@gmail.com>
2020-07-12 21:26:39 -0400

    added more rows of graphs

--- app.v4.py
+++ app.v4.py
@@ -22,9 +22,6 @@ CTP_df['date_val'] =  pd.to_datetime(CTP_df['date'], format='%Y%m%d')
 CTP_df.sort_values(['state', 'date'], inplace=True)
 CTP_df['positiveIncrease_7day'] = CTP_df.groupby('state')['positiveIncrease'].rolling(7).mean().reset_index(0,drop=True)
 CTP_df['totalTestResultsIncrease_7day'] = CTP_df.groupby('state')['totalTestResultsIncrease'].rolling(7).mean().reset_index(0,drop=True)
-CTP_df['deathIncrease_7day'] = CTP_df.groupby('state')['deathIncrease'].rolling(7).mean().reset_index(0,drop=True)
-CTP_df['hospitalizedIncrease_7day'] = CTP_df.groupby('state')['hospitalizedIncrease'].rolling(7).mean().reset_index(0,drop=True)
-CTP_df['hospitalizedCurrently_7day'] = CTP_df.groupby('state')['hospitalizedCurrently'].rolling(7).mean().reset_index(0,drop=True)
 #CTP_df.sort_values(['date', 'state'], inplace=True)
 
 census_pop = pd.read_csv('https://data.cdc.gov/api/views/b2jx-uyck/rows.csv',
@@ -47,8 +44,7 @@ cols_to_per_million = ['positive', 'negative', 'pending',
        'positiveCasesViral', 'positiveIncrease', 'negativeIncrease',
        'totalTestResults', 'totalTestResultsIncrease',
        'deathIncrease', 'hospitalizedIncrease', 
-       'totalTestResultsIncrease_7day', 'positiveIncrease_7day', 
-       'deathIncrease_7day', 'hospitalizedIncrease_7day', 'hospitalizedCurrently_7day']
+       'totalTestResultsIncrease_7day', 'positiveIncrease_7day']
 for col in cols_to_per_million:
     df[col + '_permil'] = df[col] / (df['Population'] / 1000000)
 
@@ -59,6 +55,30 @@ for index, row in df[['state', 'state_name']].drop_duplicates().fillna('NaN').it
     if (row['state_name'] != 'NaN') :
         state_options.append({'label': row['state_name'], 'value': row['state']})
 print(state_options)
+
+        #df2 = df[df['state'].isin(['NY', 'CA', 'AZ', 'FL', 'TX'])]
+        #print(df2.state.unique())
+        #print(df2[['state', 'date', 'totalTestReCovidDataPLaysultsIncrease_permil', 'positiveIncrease_permil']])
+        #print(df2['totalTestResultsIncrease_7day'].max(), df2['positiveIncrease_7day'].max())
+        #print(df2['totalTestResultsIncrease_permil'].max(), df2['positiveIncrease_permil'].max())
+        #print(CTP_df['totalTestResultsIncrease_7day'].max(), CTP_df['positiveIncrease_7day'].max())
+        # fig = px.scatter(df2, x="totalTestResultsIncrease_7day", y="positiveIncrease_7day", 
+        #                  animation_frame="date", animation_group="state", hover_name="state", facet_col='state',
+        #                  color='state', size='Population', text='state', height=800,
+        #                  range_x=[0,100000], range_y=[0,10000])
+        #CTP_df2 = CTP_df[CTP_df.state.isin(['NY', 'CA', 'AZ', 'FL', 'TX'])].copy()
+        #print(CTP_df2.state.unique())
+
+        # fig4 = px.line(df2, x="date_val", y="positiveIncrease_7day_permil", 
+        #                  hover_name="state",
+        #                  color='state', height=800,)
+
+        # fig = px.scatter(df[df['state'].isin(['NY', 'CA', 'FL', 'TX'])], 
+        #                  x="date", y="positiveIncrease_permil", 
+        #                 hover_name="state", color='state', size='Population')
+
+        # fig = px.bar(df, x="x", y=["SF", "Montreal"], barmode="group")
+        # fig.update_layout(plot_bgcolor= colors['background'], paper_bgcolor=colors['background'])
 
 app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
     html.H1(children='The Covid Tracking Project - in Dash',
@@ -75,60 +95,48 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
     dcc.Dropdown(
         id='states-dropdown',
         options=state_options,
-        value=['FL','CT', 'AZ', 'TX'],
+        value=['AZ','NY', 'TX', 'CA'],
         multi=True
     ),
+    # dcc.Dropdown(id='y-dropdown',
+    #              options=[{'label': 'Daily Pos Increase', 'value': 'positiveIncrease'},
+    #                       {'label': 'Daily Pos Increase per million 7 day', 'value': 'positiveIncrease__permil_7day'},
+    #                       {'label': 'Daily Pos Increase 7 day', 'value': 'positiveIncrease_7day'}],
+    #              value='positiveIncrease'),
 
-    #html.Div(id='states-container'),
+    html.Div(id='states-container'),
     html.Div([
         dcc.Graph(
-        id='tests-7day'    )
+        id='example-graph3'    )
         ], style={'display': 'inline-block', 'width': '49%'}),
     html.Div([
         dcc.Graph(
-        id='tests-7day_permil'    )
+        id='example-graph4'    )
         ], style={'display': 'inline-block', 'width': '49%'}),
     html.Div([
         dcc.Graph(
-        id='cases-7day'    )
+        id='example-graph5'    )
         ], style={'display': 'inline-block', 'width': '49%'}),
     html.Div([
         dcc.Graph(
-        id='cases-7day-permil'    )
-        ], style={'display': 'inline-block', 'width': '49%'}),
-    html.Div([
-        dcc.Graph(
-        id='cur_hosp-7day'    )
-        ], style={'display': 'inline-block', 'width': '49%'}),
-    html.Div([
-        dcc.Graph(
-        id='cur_hosp-7day_permil'    )
-        ], style={'display': 'inline-block', 'width': '49%'}),
-    html.Div([
-        dcc.Graph(
-        id='deaths-7day'    )
-        ], style={'display': 'inline-block', 'width': '49%'}),
-    html.Div([
-        dcc.Graph(
-        id='deaths-7day_permil'    )
-        ], style={'display': 'inline-block', 'width': '49%'}),
-
+        id='example-graph6'    )
+        ], style={'display': 'inline-block', 'width': '49%'})
 ])
 
 @app.callback(
-    dash.dependencies.Output('cases-7day', 'figure'),
+    dash.dependencies.Output('example-graph3', 'figure'),
     [dash.dependencies.Input('states-dropdown', 'value')])
 def update_output(value):
     print(value)
     df3 = df[df['state'].isin(value)]
     fig4 = px.line(df3, x="date_val", y='positiveIncrease_7day', 
                  hover_name="state",
-                 title='New Cases (7 day average)',
+                 title='Daily Values',
                  color='state', height=800,)
     return fig4
 
 @app.callback(
-    dash.dependencies.Output('cases-7day-permil', 'figure'),
+    dash.dependencies.Output('example-graph4', 'figure'),
     [dash.dependencies.Input('states-dropdown', 'value')])
 def update_output(value):
     print(value)
@@ -136,85 +144,34 @@ def update_output(value):
     df3 = df[df['state'].isin(value)]
     fig4 = px.line(df3, x="date_val", y='positiveIncrease_7day_permil', 
                  hover_name="state",
-                 title='New Cases (7 day average per million people)',
+                 title='7 day average',
                  color='state', height=800,)
+    #fig4.update_layout(hovermode='x')
     return fig4
 
 @app.callback(
-    dash.dependencies.Output('tests-7day', 'figure'),
+    dash.dependencies.Output('example-graph5', 'figure'),
     [dash.dependencies.Input('states-dropdown', 'value')])
 def update_output(value):
     print(value)
     df3 = df[df['state'].isin(value)]
     fig4 = px.line(df3, x="date_val", y='totalTestResultsIncrease_7day', 
                  hover_name="state",
-                 title='New Tests (7 day average)',
+                 title='7 day average - per million people',
                  color='state', height=800,)
     return fig4
 
 @app.callback(
-    dash.dependencies.Output('tests-7day_permil', 'figure'),
+    dash.dependencies.Output('example-graph6', 'figure'),
     [dash.dependencies.Input('states-dropdown', 'value')])
 def update_output(value):
     print(value)
     df3 = df[df['state'].isin(value)]
     fig4 = px.line(df3, x="date_val", y='totalTestResultsIncrease_7day_permil', 
                  hover_name="state",
-                 title='New Tests (7 day average per million people)',
+                 title='7 day average - per million people',
                  color='state', height=800,)
     return fig4
-
-@app.callback(
-    dash.dependencies.Output('cur_hosp-7day', 'figure'),
-    [dash.dependencies.Input('states-dropdown', 'value')])
-def update_output(value):
-    print(value)
-    df3 = df[df['state'].isin(value)]
-    fig4 = px.line(df3, x="date_val", y='hospitalizedCurrently_7day', 
-                 hover_name="state",
-                 title='Currently Hospitalized (7 day average)',
-                 color='state', height=800,)
-    return fig4
-
-@app.callback(
-    dash.dependencies.Output('cur_hosp-7day_permil', 'figure'),
-    [dash.dependencies.Input('states-dropdown', 'value')])
-def update_output(value):
-    print(value)
-    df3 = df[df['state'].isin(value)]
-    fig4 = px.line(df3, x="date_val", y='hospitalizedCurrently_7day_permil', 
-                 hover_name="state",
-                 title='Currently Hospitalized (7 day average per million people)',
-                 color='state', height=800,)
-    return fig4
-
-@app.callback(
-    dash.dependencies.Output('deaths-7day', 'figure'),
-    [dash.dependencies.Input('states-dropdown', 'value')])
-def update_output(value):
-    print(value)
-    df3 = df[df['state'].isin(value)]
-    fig4 = px.line(df3, x="date_val", y='deathIncrease_7day', 
-                 hover_name="state",
-                 title='Deaths (7 day average)',
-                 color='state', height=800,)
-    return fig4
-
-@app.callback(
-    dash.dependencies.Output('deaths-7day_permil', 'figure'),
-    [dash.dependencies.Input('states-dropdown', 'value')])
-def update_output(value):
-    print(value)
-    df3 = df[df['state'].isin(value)]
-    fig4 = px.line(df3, x="date_val", y='deathIncrease_7day_permil', 
-                 hover_name="state",
-                 title='Deaths (7 day average per million people)',
-                 color='state', height=800,)
-    return fig4
-
-
-
-# deathIncrease_7day
 
 if __name__ == '__main__':
     app.run_server(debug=True)
